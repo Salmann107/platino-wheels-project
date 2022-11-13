493eaf952ba4513a6cb3defd18d8bc65c6df0675
HAB29 <Hab-8@live.com>
2022-11-13 17:03:09 +0300

    THE ROUTES

---
+++ project-backend/routes/cars.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+const carController = require('../controllers/car')
+// We import our controller functions above
+
+
+// We define the routes and controllers
+
+
+//still i havent finish the routes
