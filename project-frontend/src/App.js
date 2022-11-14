daeaa41132c9611ad39ad7fd45a8b1611ae64736
Salmann107 <salman_almash3oo@outlook.com>
2022-11-14 22:08:04 +0300

    front-end

--- project-frontend/src/App.js
+++ project-frontend/src/App.js
@@ -1,35 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-const registerHandler = (user) => {
-  axios.post("http://localhost:4000/users", user)
-  .then(res => {
-    console.log(res);
-    })
-  .catch(err => {
-    console.log(err)
-  });
+import React from 'react';
+function App() {
+  return (
+    <div className="App">
+      <h1>aysahy</h1>
+      <p>jkjkjk</p>
+   </div>
+  );
 }
+
+// const registerHandler = (user) => {
+//   axios.post("http://localhost:4000/users", user)
+//   .then(res => {
+//     console.log(res);
+//     })
+//   .catch(err => {
+//     console.log(err)
+//   });
+// }
 
 export default App;
