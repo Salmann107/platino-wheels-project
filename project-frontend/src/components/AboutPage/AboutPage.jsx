ef2860b64d739028d114ee8e06fef0a0d2e2e9d2
HAB29 <Hab-8@live.com>
2022-11-15 13:33:48 +0300

    adding pic

---
+++ project-frontend/src/components/AboutPage/AboutPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './AboutPage.css';
+function AboutPage() {
+  return (
+    <div>
+        <div>
+    <img src="https://i.imgur.com/xYMyBt9.jpg" alt="pic" />
+    </div>
+  </div>
+  )
+}
+
+export default AboutPage
