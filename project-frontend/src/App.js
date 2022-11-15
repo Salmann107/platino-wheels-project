22bcd21bcac445495d92b1bc682a00b1aae45b74
Salmann107 <salman_almash3oo@outlook.com>
2022-11-15 14:34:47 +0300

    WRONG FILE

--- project-frontend/src/App.js
+++ project-frontend/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React from 'react'
 import NavBar from './components/NavBar/NavBar'
 import AboutPage from './components/AboutPage/AboutPage'
 import HomePage from './components/HomePage/HomePage'
