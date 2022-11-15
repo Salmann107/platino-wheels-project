df451dd2e080cfcab5e72d15a496c5b34fb47728
MramRashidR <mramrashid98@gmail.com>
2022-11-15 11:23:01 +0300

    Car model was created

---
+++ project-backend/models/Car.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const CarModel = new Schema({
+
+    photo: {type: String, required: true},
+    Brand: {type: String, required: true},
+    Year:  {type: String, required: true},
+    Price: {type: String, required: true},
+    Description: {type: String, required: true},
+    Contact: {type: String, required: true},
+
+}, {
+    timestamps: true
+})
+const Car = mongoose.model('Car' , CarModel)
+
+module.exports = Car;
