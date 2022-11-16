f26f664148031c62a3af0617cf5a43f1326c9f1c
MramRashidR <mramrashid98@gmail.com>
2022-11-16 11:06:22 +0300

    debugging

--- project-frontend/src/components/Brand/Corvette.js
+++ project-frontend/src/components/Brand/Corvette.js
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 export default function Corvette() {
+
+  const [carsList, setCarsList] = useState([])
+
+  useEffect(() => {
+    getCars()
+  }, [])
+
+
+  const getCars = () => {
+    axios.get('http://localhost:4000/cars/category/Corvette')
+    .then(res => setCarsList(res.data))
+    .catch(err => console.log(err))
+  }
+
+
   return (
-    <div>Corvette</div>
+    <div>
+
+      {carsList.length ? carsList.map(car => 
+        <div key={car._id}>
+        <p> Brand : {car.Brand}</p>
+        <p> Year : {car.Year}</p>
+        <p> Price : {car.Price}</p>
+        <p> Description : {car.Description}</p>
+        <p> Contact Information : {car.Contact}</p>
+        </div>
+        ) : null}
+
+        <br></br>
+
+
+    </div>
   )
 }
