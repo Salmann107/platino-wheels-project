55293ef9cfb7b2cf5b4eb4907356c3d9ef55fad5
MramRashidR <mramrashid98@gmail.com>
2022-11-16 11:09:14 +0300

    debugging

--- project-frontend/src/components/CategoryPage/CategoryPage.jsx
+++ project-frontend/src/components/CategoryPage/CategoryPage.jsx
@@ -10,6 +10,7 @@ function CategoryPage(props) {
             <li><Link to='/category/Ferrari'>Ferrari</Link> </li>
             <li><Link to='/category/Lamborghini'>Lamborghini</Link> </li>
             <li><Link to='/category/RollsRoyce'>Rolls Royce</Link> </li>
+            
             <li><Link to='/category/AstonMartin'>Aston Martin</Link> </li>
             <li><Link to='/category/Rivian'>Rivian</Link> </li>
             <li><Link to='/category/McLaren'>McLaren</Link> </li>
