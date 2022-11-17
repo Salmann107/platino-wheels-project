442386663a5ea4031d13346b1d2c7c51ff5fd096
HAB29 <Hab-8@live.com>
2022-11-17 13:19:43 +0300

    Final

--- project-frontend/src/components/AboutPage/AboutPage.jsx
+++ project-frontend/src/components/AboutPage/AboutPage.jsx
@@ -3,11 +3,14 @@ import './AboutPage.css';
 function AboutPage() {
   return (
 
-  <div className="background">
+  <div className="pic1">
    
     <img src= "https://i.imgur.com/igJ2a7h.jpg" alt="pic" />
-    </div>
-
+<p>Luxury cars are about status, comfort and performance. Brands like BMW, Mercedes-Benz, Audi and Maserati communicate luxury through new leather seats, expert design and the latest technology.
+It's not about the brand on the front, but the pleasure derived by driving in it. And, because luxury car buyers appreciate new technology, fresh leather seats and the status of owning a new car, they often switch cars more quickly than those who drive economy cars.
+Buying luxury cars is an experience of indulgence and pleasure, but selling your luxury car is a different story. Traditionally, you have two options: selling it yourself in a private party sale or trading it into the dealership.</p>
+<img src= "https://i.imgur.com/BlIB2YS.png" alt="pic" />
+</div>
   )
 }
 
